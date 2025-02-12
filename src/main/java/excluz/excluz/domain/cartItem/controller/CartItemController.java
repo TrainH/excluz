798507c496dfdb72f4904f27ef502ced8f982c7f
YJ-Kkang <kyujin995@gmail.com>
2025-02-12 21:15:44 +0900

    :sparkles: feat: Implement add item to cart functionality

--- src/main/java/excluz/excluz/domain/cartItem/controller/CartItemController.java
+++ src/main/java/excluz/excluz/domain/cartItem/controller/CartItemController.java
@@ -1,9 +1,17 @@
 package excluz.excluz.domain.cartItem.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import excluz.excluz.domain.cartItem.dto.request.CreateCartItemRequestDto;
+import excluz.excluz.domain.cartItem.dto.response.CreateCartItemResponseDto;
 import excluz.excluz.domain.cartItem.service.CartItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -11,4 +19,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CartItemController {
 	private final CartItemService cartItemService;
+
+	// 물품 추가
+	@PostMapping("/v1/cart-items")
+	public ResponseEntity<CreateCartItemResponseDto> addItemToCart(
+		@RequestHeader Integer userId,
+		@Valid @RequestBody CreateCartItemRequestDto requestDto
+	) {
+		// 서비스단으로 userId와 리퀘스트 정보 넘기기
+		CreateCartItemResponseDto response = cartItemService.addItemToCart(userId, requestDto);
+
+		// HTTP 상태 코드 201(create)와 함께 CreateCartItemResponseDto 응답
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
 }
--- src/main/java/excluz/excluz/domain/cartItem/service/CartItemService.java
+++ src/main/java/excluz/excluz/domain/cartItem/service/CartItemService.java
@@ -2,11 +2,34 @@ package excluz.excluz.domain.cartItem.service;
 
 import org.springframework.stereotype.Service;
 
+import excluz.excluz.common.entity.CartItem;
+import excluz.excluz.common.entity.Item;
+import excluz.excluz.common.entity.User;
+import excluz.excluz.domain.cartItem.dto.request.CreateCartItemRequestDto;
+import excluz.excluz.domain.cartItem.dto.response.CreateCartItemResponseDto;
 import excluz.excluz.domain.cartItem.repository.CartItemRepository;
+import excluz.excluz.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CartItemService {
 	private final CartItemRepository cartItemRepository;
+	private final UserRepository userRepository;
+
+	// 물품 추가
+	@Transactional
+	public CreateCartItemResponseDto addItemToCart(Integer userId, CreateCartItemRequestDto requestDto) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+		Item item = itemRepository.findById(requestDto.getItemId())
+			.orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다."));
+
+		CartItem cartItem = new CartItem(user, item, requestDto.getQuantity());
+		cartItemRepository.save(cartItem);
+
+		return new CreateCartItemResponseDto();
+	}
+
 }
