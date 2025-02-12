a28723118a5508ade54df0d9a56f24ad2a9f85f8
TrainH <ryqja18@gmail.com>
2025-02-12 15:30:28 +0900

    :tada: add orderItem entity

---
+++ src/main/java/excluz/excluz/common/entity/OrderItem.java
@@ -0,0 +1,27 @@
+package excluz.excluz.common.entity;
+
+import jakarta.persistence.*;
+
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne( fetch = FetchType.LAZY, optional = false )
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne( fetch = FetchType.LAZY, optional = false )
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer item_quantity;
+
+
+    public OrderItem(Order order, Item item, Integer itemQuantity) {
+        this.order = order;
+        this.item = item;
+        this.item_quantity = itemQuantity;
+    }
+}
