46474250920114b11fd8c1f0051dd59959047c1f
YJ-Kkang <kyujin995@gmail.com>
2025-02-12 15:09:25 +0900

    :recycle: refactor: Change ID type from Long to Integer

--- src/main/java/excluz/excluz/domain/cartItem/repository/CartItemRepository.java
+++ src/main/java/excluz/excluz/domain/cartItem/repository/CartItemRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import excluz.excluz.common.entity.CartItem;
 
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
 }
