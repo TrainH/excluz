3ba2b6904dfb39026cb24a367470532e999bb0cd
임PRO스터\su992 <su9926@naver.com>
2025-02-12 14:52:45 +0900

    :recycle:refactor:Add streamer_id FK to Store entity

--- src/main/java/excluz/excluz/common/entity/Store.java
+++ src/main/java/excluz/excluz/common/entity/Store.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,15 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "stores")
 @NoArgsConstructor
-public class Store extends BaseEntity{
+public class Store extends BaseEntity {
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer storeId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@OneToOne
+	@JoinColumn(name = "streamer_id")
+	private Streamer streamer;
 
 	@Column(length = 100)
 	private String address;
@@ -30,14 +37,14 @@ public class Store extends BaseEntity{
 	@Column(name = "is_deleted")
 	private boolean isDeleted;
 
-	public Store(String address, String storeName, String registrationNumber){
-		this.address=address;
-		this.registrationNumber=registrationNumber;
-		this.storeName=storeName;
-		this.isDeleted=false; //삭제되지 않은 상태로 설정(기본값)
+	public Store(String address, String storeName, String registrationNumber) {
+		this.address = address;
+		this.registrationNumber = registrationNumber;
+		this.storeName = storeName;
+		this.isDeleted = false; //삭제되지 않은 상태로 설정(기본값)
 	}
 
-	public void updateIsDeleted(boolean isDeleted){
-		this.isDeleted=isDeleted;
+	public void updateIsDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
 	}
 }
