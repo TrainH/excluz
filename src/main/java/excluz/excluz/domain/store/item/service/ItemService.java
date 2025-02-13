5246c23c295363fc331420d7e89670a9e5813d49
임PRO스터\su992 <su9926@naver.com>
2025-02-13 23:07:08 +0900

    :sparkles:feat:Implement createItem service logic

--- src/main/java/excluz/excluz/domain/store/item/service/ItemService.java
+++ src/main/java/excluz/excluz/domain/store/item/service/ItemService.java
@@ -1,8 +1,17 @@
 package excluz.excluz.domain.store.item.service;
 
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import excluz.excluz.common.entity.Item;
+import excluz.excluz.common.entity.Store;
+import excluz.excluz.common.exception.NotFoundException;
+import excluz.excluz.common.exception.error.ErrorCode;
+import excluz.excluz.domain.store.item.dto.request.ItemCreateRequestDto;
 import excluz.excluz.domain.store.item.repository.ItemRepository;
+import excluz.excluz.domain.store.store.repository.StoreRepository;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -10,4 +19,22 @@ import lombok.AllArgsConstructor;
 public class ItemService {
 
 	private final ItemRepository itemRepository;
+	private final StoreRepository storeRepository;
+
+	@Transactional
+	public void createItem(ItemCreateRequestDto createRequestDto, Integer streamerId) {
+		Store store = storeRepository.findStoreWithStreamer(streamerId).orElseThrow(
+			() -> new NotFoundException(ErrorCode.STORE_NOT_FOUND)
+		);
+
+		Item item = Item.builder()
+			.store(store)
+			.itemName(createRequestDto.getItemName())
+			.explanation(createRequestDto.getExplanation())
+			.price(createRequestDto.getPrice())
+			.remainingQuantity(createRequestDto.getRemainingQuantity())
+			.build();
+
+		itemRepository.save(item);
+	}
 }
