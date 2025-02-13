1d2de5c52546756e5d3ef6336c617755a48af514
임PRO스터\su992 <su9926@naver.com>
2025-02-13 16:36:35 +0900

    :sparkles:feat:Add deleteStore EndPoint

--- src/main/java/excluz/excluz/domain/store/store/controller/StoreV1Controller.java
+++ src/main/java/excluz/excluz/domain/store/store/controller/StoreV1Controller.java
@@ -2,11 +2,13 @@ package excluz.excluz.domain.store.store.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import excluz.excluz.domain.store.store.dto.request.StoreDeleteRequestDto;
 import excluz.excluz.domain.store.store.dto.request.StoreRequestDto;
 import excluz.excluz.domain.store.store.service.StoreService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,5 +33,18 @@ public class StoreV1Controller {
 		storeService.createStore(storeRequestDto, (Integer)request.getAttribute("StreamerId"));
 
 		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{storeId}")
+	public ResponseEntity<Void> deleteStore(
+		/*TODO JWT 토큰에서 streamerId 정보 추출 부분 수정 필요*/
+		HttpServletRequest request,
+		@Valid @RequestBody StoreDeleteRequestDto deleteRequestDto
+	) {
+
+		/* TODO 인증/인가 구현방식에 따라 Id 정보 추출 로직 수정 예정 */
+		storeService.deleteStore(deleteRequestDto, (Integer)request.getAttribute("StreamerId"));
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
