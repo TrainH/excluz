aa1aa365c5228cc6f7626a4688632f8cca1ad7d5
임PRO스터\su992 <su9926@naver.com>
2025-02-13 16:41:30 +0900

    :wrench:chore:Import PasswordEncoder

--- src/main/java/excluz/excluz/domain/store/store/service/StoreService.java
+++ src/main/java/excluz/excluz/domain/store/store/service/StoreService.java
@@ -3,6 +3,7 @@ package excluz.excluz.domain.store.store.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import excluz.excluz.auth.util.PasswordEncoder;
 import excluz.excluz.common.entity.Store;
 import excluz.excluz.common.entity.Streamer;
 import excluz.excluz.common.exception.BadRequestException;
@@ -19,6 +20,7 @@ public class StoreService {
 
 	private final StoreRepository storeRepository;
 	private final StreamerService streamerService;
+	private final PasswordEncoder passwordEncoder;
 
 	@Transactional
 	public void createStore(StoreRequestDto storeRequestDto, Integer streamerId) {
@@ -37,7 +39,7 @@ public class StoreService {
 	public void deleteStore(@Valid StoreDeleteRequestDto deleteRequestDto, Integer streamerId) {
 		Streamer streamer = findStreamerById(streamerId);
 
-		if(!PasswordEncoder.matches(deleteRequestDto.getPassword(), streamer.getPassword())) {
+		if(!passwordEncoder.matches(deleteRequestDto.getPassword(), streamer.getPassword())) {
 			throw new BadRequestException(ErrorCode.INVALID_PASSWORD); /*TODO 추후에 수정 예정*/
 		}
 
