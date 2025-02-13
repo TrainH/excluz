6ecd484567417480d90f889ec525c62d2cc33274
TrainH <ryqja18@gmail.com>
2025-02-13 19:46:33 +0900

    :sparkles: feat: add pointChargeRequestDto

---
+++ src/main/java/excluz/excluz/domain/point/point/dto/request/PointChargeRequestDto.java
@@ -0,0 +1,11 @@
+package excluz.excluz.domain.point.point.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record PointChargeRequestDto(
+        @NotNull(message = "충전금액을 입력하세요. ")
+        @Min(value = 1000, message = "충전금액은 1000원 이상이어야 합니다.")
+        Integer amount
+) {
+}
