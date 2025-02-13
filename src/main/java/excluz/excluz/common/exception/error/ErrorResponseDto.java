c5b5e812defe6e994660eedddc8e05d140e16861
YJ-Kkang <kyujin995@gmail.com>
2025-02-13 13:47:27 +0900

    Revert ":bug: fix(error): Remove @AllArgsConstructor to resolve constructor duplication in ErrorResponseDto"
    This reverts commit b747e5699bbf592ff7a92aaf0eb9e062fa156d2b.

--- src/main/java/excluz/excluz/common/exception/error/ErrorResponseDto.java
+++ src/main/java/excluz/excluz/common/exception/error/ErrorResponseDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class ErrorResponseDto {
 	private final HttpStatus httpStatus;
