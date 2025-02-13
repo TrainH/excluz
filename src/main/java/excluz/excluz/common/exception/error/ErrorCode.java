296fea3a518d43c966820c1301e783b5f8f4db63
임PRO스터\su992 <su9926@naver.com>
2025-02-13 23:05:05 +0900

    :sparkles:feat:Add STORE_NOT_FOUND error code

--- src/main/java/excluz/excluz/common/exception/error/ErrorCode.java
+++ src/main/java/excluz/excluz/common/exception/error/ErrorCode.java
@@ -18,8 +18,10 @@ public enum ErrorCode {
 	PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
 
 	// 아이템 관련 예외 코드
-	ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "조회되는 아이템 정보가 없습니다.");
+	ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "조회되는 아이템 정보가 없습니다."),
 
+	//스토어 관련 예외 코드
+	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "스토어 정보를 찾을 수 없습니다.");
 	// 하단에 에러코드 추가하여 사용
 
 	private final HttpStatus status;
