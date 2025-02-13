a4a52f63733912636499db2cb38dc830859b228d
임PRO스터\su992 <su9926@naver.com>
2025-02-13 15:09:58 +0900

    :sparkles:feat:Add StoreRepository

---
+++ src/main/java/excluz/excluz/domain/store/store/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package excluz.excluz.domain.store.store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import excluz.excluz.common.entity.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+}
