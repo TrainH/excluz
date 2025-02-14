c5767ae1f0850b8d068f44835c2432be5e53189a
임PRO스터\su992 <su9926@naver.com>
2025-02-14 16:38:32 +0900

    :recycle:refactor:Replace @AllArgsConstructor with explicit constructor in StoreDeleteRequestDto

--- src/main/java/excluz/excluz/domain/store/store/dto/request/StoreDeleteRequestDto.java
+++ src/main/java/excluz/excluz/domain/store/store/dto/request/StoreDeleteRequestDto.java
@@ -1,15 +1,17 @@
 package excluz.excluz.domain.store.store.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class StoreDeleteRequestDto {
 
 	@NotBlank(message = "비밀번호 입력은 필수입니다.")
 	private String password;
+
+	public StoreDeleteRequestDto(String password) {
+		this.password=password;
+	}
 }
