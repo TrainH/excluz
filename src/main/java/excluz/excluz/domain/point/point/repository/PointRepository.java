c134e53dca00bc152d5139e004fe913f95a0912a
TrainH <ryqja18@gmail.com>
2025-02-14 09:06:34 +0900

    :recycle: refactor: change find point logic in repository

--- src/main/java/excluz/excluz/domain/point/point/repository/PointRepository.java
+++ src/main/java/excluz/excluz/domain/point/point/repository/PointRepository.java
@@ -1,6 +1,7 @@
 package excluz.excluz.domain.point.point.repository;
 
 import excluz.excluz.common.entity.Point;
+import excluz.excluz.domain.user.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface PointRepository extends JpaRepository<Point, Integer> {
-    @Query("SELECT p FROM Point p WHERE p.userOrStreamerId = :userOrStreamerId")
-    Optional<Point> findByUserOrStreamerId(@Param("userOrStreamerId") Integer userOrStreamerId);
+
+    @Query("SELECT p FROM Point p WHERE p.userRole = :userRole AND p.userOrStreamerId = :userOrStreamerId")
+    Optional<Point> findByUserRoleAndUserOrStreamerId(@Param("userRole") UserRole userRole,
+                                                      @Param("userOrStreamerId") Integer userOrStreamerId);
 }
+
