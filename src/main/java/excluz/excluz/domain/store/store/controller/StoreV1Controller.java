635ac108d7332afe77fc22ca698f03913b4a2520
임PRO스터\su992 <su9926@naver.com>
2025-02-15 00:13:03 +0900

    :sparkles:feat:Add authentication to StoreController
    - Use @AuthenticationPrincipal to get the authenticated user
    - Extract streamerId from User object

--- src/main/java/excluz/excluz/domain/store/store/controller/StoreV1Controller.java
+++ src/main/java/excluz/excluz/domain/store/store/controller/StoreV1Controller.java
@@ -2,6 +2,8 @@ package excluz.excluz.domain.store.store.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,26 +31,23 @@ public class StoreV1Controller {
 
 	@PostMapping()
 	public ResponseEntity<Void> createStore(
-		/*TODO JWT 토큰에서 streamerId 정보 추출. @AuthenticationPrincipal AuthUser user 쓰시려나?*/
-		HttpServletRequest request,
+		@AuthenticationPrincipal User user,
 		@Valid @RequestBody StoreRequestDto storeRequestDto
 	) {
 
-		/* TODO 인증/인가 구현방식에 따라 Id 정보 추출 로직 수정 예정 */
-		storeService.createStore(storeRequestDto, (Integer)request.getAttribute("StreamerId"));
+		Integer streamerId = Integer.valueOf(user.getUsername());
+		storeService.createStore(storeRequestDto, streamerId);
 
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{storeId}")
 	public ResponseEntity<Void> deleteStore(
-		/*TODO JWT 토큰에서 streamerId 정보 추출 부분 수정 필요*/
-		HttpServletRequest request,
+		@AuthenticationPrincipal User user,
 		@Valid @RequestBody StoreDeleteRequestDto deleteRequestDto
 	) {
-
-		/* TODO 인증/인가 구현방식에 따라 Id 정보 추출 로직 수정 예정 */
-		storeService.deleteStore(deleteRequestDto, (Integer)request.getAttribute("StreamerId"));
+		Integer streamerId = Integer.valueOf(user.getUsername());
+		storeService.deleteStore(deleteRequestDto, streamerId);
 
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
