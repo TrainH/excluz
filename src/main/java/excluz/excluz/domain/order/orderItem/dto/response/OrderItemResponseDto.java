e49f43c332f8019dbb15559ae33fd9561a63f643
TrainH <ryqja18@gmail.com>
2025-02-14 14:37:38 +0900

    :recycle: refactor: Change record to class in OrderItemResponseDto

--- src/main/java/excluz/excluz/domain/order/orderItem/dto/response/OrderItemResponseDto.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/dto/response/OrderItemResponseDto.java
@@ -2,22 +2,35 @@ package excluz.excluz.domain.order.orderItem.dto.response;
 
 import excluz.excluz.common.entity.OrderItem;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
-public record OrderItemResponseDto (
-        String nickName,
-        Integer itemId,
-        String itemName,
-        Integer price,
-        Integer itemQuantity
-) {
+@Getter
+@NoArgsConstructor
+public class OrderItemResponseDto {
+
+    private String nickName;
+    private Integer itemId;
+    private String itemName;
+    private Integer price;
+    private Integer itemQuantity;
+
+    @Builder
+    public OrderItemResponseDto(String nickName, Integer itemId, String itemName, Integer price, Integer itemQuantity) {
+        this.nickName = nickName;
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.price = price;
+        this.itemQuantity = itemQuantity;
+    }
+
     public static OrderItemResponseDto from(OrderItem orderItem) {
-        return new OrderItemResponseDto(
-                orderItem.getItem().getStore().getStreamer().getNickName(),
-                orderItem.getItem().getStore().getId(),
-                orderItem.getItem().getItemName(),
-                orderItem.getItem().getPrice(),
-                orderItem.getItem_quantity()
-        );
+        return OrderItemResponseDto.builder()
+                .nickName(orderItem.getItem().getStore().getStreamer().getNickName())
+                .itemId(orderItem.getItem().getId())
+                .itemName(orderItem.getItem().getItemName())
+                .price(orderItem.getItem().getPrice())
+                .itemQuantity(orderItem.getItem_quantity())
+                .build();
     }
 }
