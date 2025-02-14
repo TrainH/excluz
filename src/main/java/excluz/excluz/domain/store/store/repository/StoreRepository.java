4a467c1256c8753b1dca9ec935df689cf8d57cc9
임PRO스터\su992 <su9926@naver.com>
2025-02-15 01:19:47 +0900

    :sparkles:feat:Implement findByStoreName query to search non-deleted stores

--- src/main/java/excluz/excluz/domain/store/store/repository/StoreRepository.java
+++ src/main/java/excluz/excluz/domain/store/store/repository/StoreRepository.java
@@ -2,6 +2,8 @@ package excluz.excluz.domain.store.store.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,10 @@ public interface StoreRepository extends JpaRepository<Store, Integer> {
 
 	@Query("SELECT s FROM Store s JOIN FETCH s.streamer WHERE s.streamer.id = :streamerId")
 	Optional<Store> findStoreWithStreamer(@Param("streamerId") Integer streamerId);
+
+	@Query("SELECT s FROM Store s " +
+		"WHERE s.storeName LIKE CONCAT('%', :storeName, '%') " +
+		"AND s.isDeleted=false " +
+		"ORDER BY s.id DESC")
+	Page<Store> findByStoreName(Pageable pageable, @Param("storeName") String storeName);
 }
