3ffd6e1c7862b07b943ce1922410c6d62631d97f
JunChoi <chldnjswns1@daum.net>
2025-02-14 14:37:02 +0900

    :recycle:refactor:Follow Convention(@Positive → @Min(value = 1)

--- src/main/java/excluz/excluz/domain/event/eventItem/dto/EventItemRequestDto.java
+++ src/main/java/excluz/excluz/domain/event/eventItem/dto/EventItemRequestDto.java
@@ -1,6 +1,7 @@
 package excluz.excluz.domain.event.eventItem.dto;
 
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
@@ -14,7 +15,7 @@ public class EventItemRequestDto {
     private Integer itemId;
 
     @NotNull(message = "상품 수량은 필수입니다.")
-    @Positive(message = "상품 수량은 1개 이상이어야 합니다.")
+    @Min(value = 1, message = "상품 수량은 1개 이상이어야 합니다.")
     private Integer quantity;
 
     @Builder
