e23842d0ad446e36ec864c8989611f5d579b2828
TrainH <ryqja18@gmail.com>
2025-02-14 13:12:44 +0900

    :recycle: refactor: Add minimum condition of itemQuantity in OrderItemRequestDto

--- src/main/java/excluz/excluz/domain/order/orderItem/dto/request/OrderItemRequestDto.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/dto/request/OrderItemRequestDto.java
@@ -1,5 +1,6 @@
 package excluz.excluz.domain.order.orderItem.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,6 +9,7 @@ public record OrderItemRequestDto(
         Integer itemId,
 
         @NotNull(message = "itemQuantity 는 필수 입력값입니다.")
+        @Min(value = 1, message = "itemQuantity 는 1 이상이어야 합니다.")
         Integer itemQuantity,
 
         @NotBlank(message = "address 는 필수 입력값입니다.")
