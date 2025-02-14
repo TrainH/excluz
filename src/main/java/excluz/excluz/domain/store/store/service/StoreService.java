90ace98f8fd4b30ea0fbd8fd8829b1446f8b730f
임PRO스터\su992 <su9926@naver.com>
2025-02-14 16:13:00 +0900

    :sparkles:feat:Prevent deleted users from creating a store

--- src/main/java/excluz/excluz/domain/store/store/service/StoreService.java
+++ src/main/java/excluz/excluz/domain/store/store/service/StoreService.java
@@ -5,8 +5,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import excluz.excluz.common.entity.Store;
 import excluz.excluz.common.entity.Streamer;
+import excluz.excluz.common.exception.NotFoundException;
+import excluz.excluz.common.exception.error.ErrorCode;
 import excluz.excluz.domain.store.store.dto.request.StoreRequestDto;
 import excluz.excluz.domain.store.store.repository.StoreRepository;
+import excluz.excluz.domain.streamer.service.StreamerService;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,7 +21,13 @@ public class StoreService {
 
 	@Transactional
 	public void createStore(StoreRequestDto storeRequestDto, Integer streamerId) {
-		Streamer streamer = findStreamerById(streamerId);
+		Streamer streamer = streamerService.findStreamerById(streamerId);
+
+		// 삭제된 유저는 스토어 생성 불가
+		if (streamer.isDeleted()) {
+			throw new NotFoundException(ErrorCode.USER_NOT_FOUND);
+		}
+
 		Store store = Store.builder()
 			.streamer(streamer)
 			.address(storeRequestDto.getAddress())
