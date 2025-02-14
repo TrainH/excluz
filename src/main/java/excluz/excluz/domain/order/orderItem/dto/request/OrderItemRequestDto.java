5798a61ca9497e584c82cb1b23fc3c2a6ef7ba50
TrainH <ryqja18@gmail.com>
2025-02-14 14:36:38 +0900

    :recycle: refactor: Change record to class in OrderItemResponseDto

--- src/main/java/excluz/excluz/domain/order/orderItem/dto/request/OrderItemRequestDto.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/dto/request/OrderItemRequestDto.java
@@ -3,16 +3,26 @@ package excluz.excluz.domain.order.orderItem.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record OrderItemRequestDto(
+@Getter
+@NoArgsConstructor
+public class OrderItemRequestDto {
+
         @NotNull(message = "itemId 는 필수 입력값입니다.")
-        Integer itemId,
+        private Integer itemId;
 
         @NotNull(message = "itemQuantity 는 필수 입력값입니다.")
         @Min(value = 1, message = "itemQuantity 는 1 이상이어야 합니다.")
-        Integer itemQuantity,
+        private Integer itemQuantity;
 
         @NotBlank(message = "address 는 필수 입력값입니다.")
-        String address
-) {
+        private String address;
+
+        public OrderItemRequestDto(Integer itemId, Integer itemQuantity, String address) {
+                this.itemId = itemId;
+                this.itemQuantity = itemQuantity;
+                this.address = address;
+        }
 }
