8538d307b78ffeb582f02d0f8f68d5eeada9c01b
TrainH <ryqja18@gmail.com>
2025-02-14 17:46:48 +0900

    :sparkles: feat: add getPointTransactionList

---
+++ src/main/java/excluz/excluz/domain/point/pointTransaction/controller/PointTransactionContoller.java
@@ -0,0 +1,30 @@
+package excluz.excluz.domain.point.pointTransaction.controller;
+
+import excluz.excluz.domain.point.pointTransaction.dto.response.PointTransactionResponseDto;
+import excluz.excluz.domain.point.pointTransaction.service.PointTransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class PointTransactionContoller {
+    private final PointTransactionService pointTransactionService;
+
+    @GetMapping("/points/transactions")
+    public ResponseEntity<Page<PointTransactionResponseDto>> getPointTransactions(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Pageable pageable = PageRequest.of(page-1, size);
+
+        return ResponseEntity.ok(pointTransactionService.getPointTransactionList(pageable));
+    }
+}
---
+++ src/main/java/excluz/excluz/domain/point/pointTransaction/dto/response/PointTransactionResponseDto.java
@@ -0,0 +1,42 @@
+package excluz.excluz.domain.point.pointTransaction.dto.response;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import excluz.excluz.common.entity.PointTransaction;
+import excluz.excluz.domain.point.pointTransaction.enums.TransactionType;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class PointTransactionResponseDto {
+    private String userNickName;
+    private String streamerNickName;
+    private TransactionType transactionType;
+    private Integer amount;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public PointTransactionResponseDto(String userNickName,
+                                       String streamerNickName,
+                                       TransactionType transactionType,
+                                       Integer amount,
+                                       LocalDateTime createdAt) {
+        this.userNickName = userNickName;
+        this.streamerNickName = streamerNickName;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.createdAt = createdAt;
+    }
+
+    public static PointTransactionResponseDto from(PointTransaction pointTransaction) {
+        return PointTransactionResponseDto.builder()
+                .userNickName(pointTransaction.getUser().getNickName())
+                .streamerNickName(pointTransaction.getStore().getStreamer().getNickName())
+                .transactionType(pointTransaction.getTransactionType())
+                .amount(pointTransaction.getAmount())
+                .createdAt(pointTransaction.getCreatedAt())
+                .build();
+    }
+}
