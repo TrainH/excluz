47621b3ff7ac156011963681a58e6012cf490fc8
임PRO스터\su992 <su9926@naver.com>
2025-02-15 15:12:29 +0900

    :recycle:refactor:Update HTTP method and URL for deleteItem endpoint

--- src/main/java/excluz/excluz/domain/store/item/controller/ItemV1Controller.java
+++ src/main/java/excluz/excluz/domain/store/item/controller/ItemV1Controller.java
@@ -3,6 +3,7 @@ package excluz.excluz.domain.store.item.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +36,7 @@ public class ItemV1Controller {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
-	@DeleteMapping("/{itemsId}")
+	@PatchMapping("/{itemsId}/disable")
 	public ResponseEntity<Void> deleteItem(@PathVariable Integer itemsId) {
 
 		itemService.deleteItem(itemsId);
