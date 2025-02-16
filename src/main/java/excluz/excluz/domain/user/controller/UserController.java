d2be1b658a929de1c641889c06cc23d49f623137
neulbin <msg0447@gmail.com>
2025-02-17 01:41:10 +0900

    :sparkles: feat: Add user withdrawal API and service logic

--- src/main/java/excluz/excluz/domain/user/controller/UserController.java
+++ src/main/java/excluz/excluz/domain/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package excluz.excluz.domain.user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import excluz.excluz.domain.user.dto.request.UserLoginRequestDto;
 import excluz.excluz.domain.user.dto.request.UserSignupRequestDto;
+import excluz.excluz.domain.user.dto.request.UserWithdrawRequestDto;
 import excluz.excluz.domain.user.dto.response.UserLoginResponseDto;
 import excluz.excluz.domain.user.dto.response.UserSignupResponseDto;
+import excluz.excluz.domain.user.dto.response.UserWithdrawResponseDto;
 import excluz.excluz.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -38,5 +43,18 @@ public class UserController {
 		UserLoginResponseDto userLoginResponseDto = userService.userLogin(loginRequest);
 
 		return ResponseEntity.ok(userLoginResponseDto);
+	}
+
+	@PatchMapping("/withdraw")
+	public ResponseEntity<UserWithdrawResponseDto> userUnregisterAPI(
+		@AuthenticationPrincipal User user,
+		@RequestBody UserWithdrawRequestDto userWithdrawRequest) {
+
+		// 스트링 값으로 저장된 userId를 Integer 로 강제 반환
+		Integer userId = Integer.parseInt(user.getUsername());
+
+		UserWithdrawResponseDto userWithdrawResponseDto = userService.userWithdraw(userId, userWithdrawRequest);
+
+		return ResponseEntity.ok(userWithdrawResponseDto);
 	}
 }
--- src/main/java/excluz/excluz/domain/user/service/UserService.java
+++ src/main/java/excluz/excluz/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import excluz.excluz.auth.util.JwtUtil;
 import excluz.excluz.common.entity.User;
@@ -12,8 +13,10 @@ import excluz.excluz.common.exception.NotFoundException;
 import excluz.excluz.common.exception.error.ErrorCode;
 import excluz.excluz.domain.user.dto.request.UserLoginRequestDto;
 import excluz.excluz.domain.user.dto.request.UserSignupRequestDto;
+import excluz.excluz.domain.user.dto.request.UserWithdrawRequestDto;
 import excluz.excluz.domain.user.dto.response.UserLoginResponseDto;
 import excluz.excluz.domain.user.dto.response.UserSignupResponseDto;
+import excluz.excluz.domain.user.dto.response.UserWithdrawResponseDto;
 import excluz.excluz.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -74,5 +77,29 @@ public class UserService {
 
 		// 로그인 완료 메세지 및 토큰값 발행
 		return new UserLoginResponseDto("로그인 되었습니다.", token);
+	}
+
+	// 회원탈퇴
+	@Transactional
+	public UserWithdrawResponseDto userWithdraw(
+		Integer userId, UserWithdrawRequestDto userWithdrawRequestDto) {
+
+		// 유저 정보를 userId 로 조회
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+		// 리퀘스트 요청에 들어온 비밀번호와 재확인 비밀번호가 일치 하지 않을 시 예외
+		if (!userWithdrawRequestDto.getPassword().equals(userWithdrawRequestDto.getReEnterPassword())) {
+			throw new BadRequestException(ErrorCode.PASSWORD_MISMATCH);
+		}
+
+		// 비밀번호 검증 로직
+		if(!passwordEncoder.matches(userWithdrawRequestDto.getPassword(), user.getPassword())) {
+			throw new BadRequestException(ErrorCode.PASSWORD_MISMATCH);
+		}
+
+		user.updateUserStatus(true);
+
+		return new UserWithdrawResponseDto("회원탈퇴가 완료되었습니다 저희 서비스를 이용해주셔서 감사합니다.");
 	}
 }
