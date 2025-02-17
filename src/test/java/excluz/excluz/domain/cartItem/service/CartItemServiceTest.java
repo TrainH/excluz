17efd747236cb2707cea1b825dd49af470a665f1
YJ-Kkang <kyujin995@gmail.com>
2025-02-17 17:57:25 +0900

    :white_check_mark: test: Add unit test for adding an item to the cart

--- src/test/java/excluz/excluz/domain/cartItem/service/CartItemServiceTest.java
+++ src/test/java/excluz/excluz/domain/cartItem/service/CartItemServiceTest.java
@@ -1,5 +1,7 @@
 package excluz.excluz.domain.cartItem.service;
 
+import static org.mockito.Mockito.*;
+
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -10,56 +12,76 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import excluz.excluz.common.entity.CartItem;
 import excluz.excluz.common.entity.Item;
+import excluz.excluz.common.entity.User;
 import excluz.excluz.common.exception.BadRequestException;
+import excluz.excluz.domain.cartItem.dto.request.CreateCartItemRequestDto;
 import excluz.excluz.domain.cartItem.dto.request.UpdateCartItemQuantityRequestDto;
+import excluz.excluz.domain.cartItem.dto.response.CreateCartItemResponseDto;
 import excluz.excluz.domain.cartItem.repository.CartItemRepository;
+import excluz.excluz.domain.store.item.repository.ItemRepository;
+import excluz.excluz.domain.user.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 class CartItemServiceTest {
 
+	// 가짜 객체
 	@Mock
 	private CartItemRepository cartItemRepository;
 
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private ItemRepository itemRepository;
+
+	// 실제 객체
 	@InjectMocks
 	private CartItemService cartItemService;
 
 	@Test
-	@DisplayName("fail: 요청 개수 > 재고 (예외 발생)")
-	void updateCartItemQuantity() {
+	@DisplayName("success: 장바구니에 아이템 추가 메서드")
+	void addItemToCart_success() {
 		// given
+		User user = mock(User.class);
 		Item item = new Item(
 			null,         // store: null (스토어 정보 없음)
 			"itemName",   // itemName: "itemName" (상품명)
 			"test",       // explanation: "test" (설명)
 			100,          // price: 100 (상품 가격)
-			10            // remainingQuantity: 10 (재고 개수)
+			100            // remainingQuantity: 100 (재고 개수)
 		);
+
+		Integer userId = 1;
+		CreateCartItemRequestDto requestDto = new CreateCartItemRequestDto(
+			1, // itemId: 1 (아이템 아이디)
+			10 // quantity: 10 (장바구니에 담는 물건 개수)
+		);
+
+		when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+		when(itemRepository.findById(requestDto.getItemId())).thenReturn(Optional.of(item));
+
 		CartItem cartItem = new CartItem(
-			null,  // user: null (사용자 정보 없음)
-			item,  // item: 위에서 생성한 Item 객체
-			10     // quantity: 10 (현재 장바구니에 담긴 수량)
+			user,
+			item,
+			requestDto.getQuantity()
 		);
 
-		Mockito.when(cartItemRepository.findByIdAndUserId(Mockito.any(), Mockito.any()))
-			.thenReturn(Optional.of(cartItem));
+		when(cartItemRepository.save(any(CartItem.class))).thenReturn(cartItem);
 
-		UpdateCartItemQuantityRequestDto cartItemQuantityRequestDto = new UpdateCartItemQuantityRequestDto(100);
+		// when
+		CreateCartItemResponseDto result = cartItemService.addItemToCart(userId, requestDto);
 
-		// when, then
-		Assertions.assertThatThrownBy(() -> cartItemService.updateCartItemQuantity(
-				null,   // user: null (사용자 정보 없음)
-				null,   // cartItem: null (장바구니 아이템 정보 없음)
-				cartItemQuantityRequestDto // 업데이트할 장바구니 아이템 정보
-			))
-			.isInstanceOf(BadRequestException.class);
+		// than
+		Assertions.assertThat(result).isNotNull(); // 반환값이 null이 아닌지 확인
 	}
 
 	@Test
 	@DisplayName("success: 요청 개수 < 재고")
-	void updateCartItemQuantity2() {
+	void updateCartItemQuantity_success() {
 		// given
 		Item item = new Item(
 			null,         // store: null (스토어 정보 없음)
@@ -86,5 +108,36 @@ class CartItemServiceTest {
 				cartItemQuantityRequestDto // 업데이트할 장바구니 아이템 정보
 			))
 			.doesNotThrowAnyException();
+	}
+
+	@Test
+	@DisplayName("fail: 요청 개수 > 재고 (예외 발생)")
+	void updateCartItemQuantity_fail() {
+		// given
+		Item item = new Item(
+			null,         // store: null (스토어 정보 없음)
+			"itemName",   // itemName: "itemName" (상품명)
+			"test",       // explanation: "test" (설명)
+			100,          // price: 100 (상품 가격)
+			10            // remainingQuantity: 10 (재고 개수)
+		);
+		CartItem cartItem = new CartItem(
+			null,  // user: null (사용자 정보 없음)
+			item,  // item: 위에서 생성한 Item 객체
+			10     // quantity: 10 (현재 장바구니에 담긴 수량)
+		);
+
+		Mockito.when(cartItemRepository.findByIdAndUserId(Mockito.any(), Mockito.any()))
+			.thenReturn(Optional.of(cartItem));
+
+		UpdateCartItemQuantityRequestDto cartItemQuantityRequestDto = new UpdateCartItemQuantityRequestDto(100);
+
+		// when, then
+		Assertions.assertThatThrownBy(() -> cartItemService.updateCartItemQuantity(
+				null,   // user: null (사용자 정보 없음)
+				null,   // cartItem: null (장바구니 아이템 정보 없음)
+				cartItemQuantityRequestDto // 업데이트할 장바구니 아이템 정보
+			))
+			.isInstanceOf(BadRequestException.class);
 	}
 }
