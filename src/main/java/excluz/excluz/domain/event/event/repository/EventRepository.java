89620b95089b22392a2fb172f8e472a604960f41
JunChoi <chldnjswns1@daum.net>
2025-02-18 20:19:28 +0900

    :sparkles:feat:Add Pessimistic Lock

--- src/main/java/excluz/excluz/domain/event/event/repository/EventRepository.java
+++ src/main/java/excluz/excluz/domain/event/event/repository/EventRepository.java
@@ -1,10 +1,17 @@
 package excluz.excluz.domain.event.event.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import excluz.excluz.common.entity.Event;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    Optional<Event> findByGeneratedCode(String generatedCode);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM Event e WHERE e.generatedCode = :code")
+    Optional<Event> findByGeneratedCode(@Param("code") String generatedCode);
 }
