a5273aaa34dcb02a061c11dbdfc3a4e37dbc0287
임PRO스터\su992 <su9926@naver.com>
2025-02-18 16:34:47 +0900

    :white_check_mark:test:Refactor test structure with @Nested and improved organization

--- src/test/java/excluz/excluz/domain/store/item/service/ItemServiceTest.java
+++ src/test/java/excluz/excluz/domain/store/item/service/ItemServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import excluz.excluz.common.datas.SharedData;
 import excluz.excluz.common.entity.Item;
 import excluz.excluz.common.entity.Store;
 import excluz.excluz.common.entity.Streamer;
@@ -28,44 +29,8 @@ import excluz.excluz.domain.streamer.dto.request.StreamerLoginRequestDto;
 import excluz.excluz.domain.streamer.dto.request.StreamerSignupRequestDto;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("ItemService의")
 class ItemServiceTest {
-
-	/* 공유 데이터 */
-	// Streamer 데이터
-	public final static Integer TEST_STREAMER_ID1 = 1;
-	public final static String TEST_STREAMER_NAME1 = "홍길동";
-	public final static String TEST_STREAMER_NICKNAME1 = "암행어사";
-	public final static String TEST_STREAMER_PHONE_NUMBER1 = "010-1234-1234";
-	public final static String TEST_STREAMER_EMAIL1 = "test12@test.com";
-	public final static String TEST_STREAMER_PASSWORD1 = "Qwer1234!!!!";
-	public final static String TEST_STREAMER_REENTER_PASSWORD1 = "Qwer1234!!!!";
-	public final static Streamer TEST_STREAMER1 = new Streamer(TEST_STREAMER_NAME1, TEST_STREAMER_NICKNAME1, TEST_STREAMER_PHONE_NUMBER1, TEST_STREAMER_EMAIL1, TEST_STREAMER_PASSWORD1);
-	public final static StreamerSignupRequestDto TEST_STREAMER_SIGNUP_REQUEST_DTO = new StreamerSignupRequestDto(TEST_STREAMER_NAME1, TEST_STREAMER_NICKNAME1, TEST_STREAMER_PHONE_NUMBER1, TEST_STREAMER_EMAIL1, TEST_STREAMER_PASSWORD1, TEST_STREAMER_REENTER_PASSWORD1);
-	public final static StreamerLoginRequestDto TEST_STREAMER_LOGIN_REQUEST_DTO = new StreamerLoginRequestDto(TEST_STREAMER_EMAIL1, TEST_STREAMER_PASSWORD1);
-
-	// Store 데이터
-	public final static Integer TEST_STORE_ID1 = 1;
-	public final static String TEST_ADDRESS1 = "사랑시 평화동 정의로12";
-	public final static String TEST_STORE_NAME1 = "테스트샵";
-	public final static String TEST_REGISTRATION_NUMBER1 = "123-12-12345";
-	public final static Store TEST_STORE1 = new Store(TEST_STREAMER1, TEST_ADDRESS1, TEST_STORE_NAME1,TEST_REGISTRATION_NUMBER1);
-
-	// Item 데이터
-	public final static Integer TEST_ITEM_ID1 = 1;
-	public final static String TEST_ITEM_NAME1 = "테스트 굿즈";
-	public final static String TEST_ITEM_NAME2 = "업데이트 굿즈";
-	public final static String TEST_EXPLANATION1 = "굿즈 설명";
-	public final static String TEST_EXPLANATION2 = "업데이트 굿즈 설명";
-	public final static Integer TEST_PRICE1 = 3000;
-	public final static Integer TEST_PRICE2 = 4000;
-	public final static Integer TEST_REMAINING_QUANTITY1 = 100;
-	public final static Integer TEST_REMAINING_QUANTITY2 = 200;
-	public final static Item TEST_UPDATED_ITEM = new Item(TEST_STORE1, TEST_ITEM_NAME2, TEST_EXPLANATION2, TEST_PRICE2, TEST_REMAINING_QUANTITY2);
-	public final static Item TEST_ITEM1 = new Item(TEST_STORE1, TEST_ITEM_NAME1, TEST_EXPLANATION1, TEST_PRICE1, TEST_REMAINING_QUANTITY1);
-	public final static Item TEST_ITEM2 = new Item(TEST_STORE1, TEST_ITEM_NAME2, TEST_EXPLANATION2, TEST_PRICE2, TEST_REMAINING_QUANTITY2);
-	public final static ItemCreateRequestDto TEST_ITEM_CREATE_REQUEST_DTO = new ItemCreateRequestDto(TEST_ITEM_NAME1, TEST_EXPLANATION1, TEST_PRICE1, TEST_REMAINING_QUANTITY1);
-	public final static ItemUpdateRequestDto TEST_ITEM_UPDATE_REQUEST_DTO = new ItemUpdateRequestDto(TEST_ITEM_NAME2, TEST_EXPLANATION2, TEST_PRICE2, TEST_REMAINING_QUANTITY2);
-	public final static ItemResponseDto TEST_ITEM_RESPONSE_DTO = new ItemResponseDto(TEST_ITEM_NAME2, TEST_EXPLANATION2, TEST_PRICE2, TEST_REMAINING_QUANTITY2);
 
 	@InjectMocks
 	ItemService itemService;
@@ -82,8 +47,46 @@ class ItemServiceTest {
 	Streamer mockStreamer;
 
 	@Nested
-	@DisplayName("기본 설정이 필요한 테스트")
-	class WithStubbing {
+	@DisplayName("UpdateItem 메서드는")
+	class UpdateItem {
+
+		@BeforeEach
+		void setUp() {
+			when(mockItem.getStore()).thenReturn(mockStore);
+			when(mockStore.getStreamer()).thenReturn(mockStreamer);
+			when(mockStreamer.getId()).thenReturn(1);
+		}
+
+		@Test
+		@DisplayName("success: 아이템 정보 수정 성공")
+		void update_ItemInfo_success() {
+			// given
+			Item updatedItem = SharedData.UPDATED_ITEM;
+
+			when(itemRepository.findItemByIdAndNotDeleted(anyInt())).thenReturn(Optional.of(mockItem));
+			when(mockItem.getStore().getStreamer().getId()).thenReturn(1);
+
+			try (MockedStatic<ItemResponseDto> itemMockedStatic = mockStatic(ItemResponseDto.class)) {
+				given(ItemResponseDto.from(any(Item.class))).willReturn(SharedData.ITEM_RESPONSE_DTO);
+
+				// when
+				ItemResponseDto actualResult = itemService.updateItemInfo(SharedData.ITEM_UPDATE_REQUEST_DTO, SharedData.ITEM_ID1,
+					SharedData.STREAMER_ID1);
+
+				// then
+				verify(mockItem, times(1)).updateItem(anyString(), anyString(), anyInt(), anyInt());
+
+				assertThat(actualResult.getItemName()).isEqualTo(updatedItem.getItemName());
+				assertThat(actualResult.getExplanation()).isEqualTo(updatedItem.getExplanation());
+				assertThat(actualResult.getPrice()).isEqualTo(updatedItem.getPrice());
+				assertThat(actualResult.getRemainingQuantity()).isEqualTo(updatedItem.getRemainingQuantity());
+			}
+		}
+	}
+
+	@Nested
+	@DisplayName("deleteItem 메서드는")
+	class DeleteItem {
 
 		@BeforeEach
 		void setUp() {
@@ -100,67 +103,28 @@ class ItemServiceTest {
 			when(mockItem.getStore().getStreamer().getId()).thenReturn(1);
 
 			// when
-			itemService.deleteItem(TEST_ITEM_ID1, TEST_STREAMER_ID1);
+			itemService.deleteItem(SharedData.ITEM_ID1, SharedData.STREAMER_ID1);
 
 			// then
 			verify(mockItem, times(1)).updateIsDeleted(true);
 		}
 
-		@Test
-		@DisplayName("success: 아이템 정보 수정 성공")
-		void update_ItemInfo_success() {
-			// given
-			Item updatedItem = TEST_UPDATED_ITEM;
-
-			when(itemRepository.findItemByIdAndNotDeleted(anyInt())).thenReturn(Optional.of(mockItem));
-			when(mockItem.getStore().getStreamer().getId()).thenReturn(1);
-
-			try (MockedStatic<ItemResponseDto> itemMockedStatic = mockStatic(ItemResponseDto.class)) {
-				given(ItemResponseDto.from(any(Item.class))).willReturn(TEST_ITEM_RESPONSE_DTO);
-
-				// when
-				ItemResponseDto actualResult = itemService.updateItemInfo(TEST_ITEM_UPDATE_REQUEST_DTO, TEST_ITEM_ID1,
-					TEST_STREAMER_ID1);
-
-				// then
-				verify(mockItem, times(1)).updateItem(anyString(), anyString(), anyInt(), anyInt());
-
-				assertThat(actualResult.getItemName()).isEqualTo(updatedItem.getItemName());
-				assertThat(actualResult.getExplanation()).isEqualTo(updatedItem.getExplanation());
-				assertThat(actualResult.getPrice()).isEqualTo(updatedItem.getPrice());
-				assertThat(actualResult.getRemainingQuantity()).isEqualTo(updatedItem.getRemainingQuantity());
-			}
-		}
 	}
 
 	@Nested
-	@DisplayName("기본 설정이 불필요한 테스트")
-	class WithoutStubbing {
-
-		@Test
-		@DisplayName("success: 아이템 생성 기능 정상 수행")
-		void createItem_success() {
-			// given
-			when(storeRepository.findStoreWithStreamer(TEST_STREAMER_ID1)).thenReturn(Optional.of(TEST_STORE1));
-			when(itemRepository.save(any(Item.class))).thenReturn(TEST_ITEM1);
-
-			// when
-			itemService.createItem(TEST_ITEM_CREATE_REQUEST_DTO, TEST_STREAMER_ID1);
-
-			// then
-			verify(itemRepository, times(1)).save(any(Item.class));
-		}
+	@DisplayName("getItemById 메서드는")
+	class GetItemById {
 
 		@Test
 		@DisplayName("success: itemId로 아이템 단건 조회")
 		void getItemById_success() {
 			// given
-			Item item = TEST_ITEM2;
+			Item item = SharedData.ITEM2;
 
 			when(itemRepository.findItemByIdAndNotDeleted(eq(item.getId()))).thenReturn(Optional.of(item));
 
 			try (MockedStatic<ItemResponseDto> itemMockedStatic = mockStatic(ItemResponseDto.class)) {
-				given(ItemResponseDto.from(any(Item.class))).willReturn(TEST_ITEM_RESPONSE_DTO);
+				given(ItemResponseDto.from(any(Item.class))).willReturn(SharedData.ITEM_RESPONSE_DTO);
 
 				// when
 				ItemResponseDto actualResult = itemService.getItemById(item.getId());
@@ -171,6 +135,25 @@ class ItemServiceTest {
 				assertThat(actualResult.getPrice()).isEqualTo(item.getPrice());
 				assertThat(actualResult.getRemainingQuantity()).isEqualTo(item.getRemainingQuantity());
 			}
+		}
+	}
+
+	@Nested
+	@DisplayName("createItem 메서드는")
+	class CreateItem {
+
+		@Test
+		@DisplayName("success: 아이템 생성 기능 정상 수행")
+		void createItem_success() {
+			// given
+			when(storeRepository.findStoreWithStreamer(1)).thenReturn(Optional.of(SharedData.STORE1));
+			when(itemRepository.save(any(Item.class))).thenReturn(SharedData.ITEM1);
+
+			// when
+			itemService.createItem(SharedData.ITEM_CREATE_REQUEST_DTO, 1);
+
+			// then
+			verify(itemRepository, times(1)).save(any(Item.class));
 		}
 	}
 }
