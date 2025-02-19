9334e3e6f1edbd3245657ecadb297ea83189f026
TrainH <ryqja18@gmail.com>
2025-02-19 15:26:37 +0900

    :recycle: refactor: auth in OrderItemController

--- src/main/java/excluz/excluz/domain/order/orderItem/controller/OrderItemController.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/controller/OrderItemController.java
@@ -1,13 +1,16 @@
 package excluz.excluz.domain.order.orderItem.controller;
 
+import excluz.excluz.auth.util.SecurityContextUtil;
 import excluz.excluz.domain.order.orderItem.dto.request.OrderItemRequestDto;
 import excluz.excluz.domain.order.orderItem.dto.response.OrderItemResponseDto;
 import excluz.excluz.domain.order.orderItem.service.OrderItemService;
+import excluz.excluz.domain.user.enums.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -22,15 +25,12 @@ public class OrderItemController {
     private final OrderItemService orderItemService;
 
     @PostMapping("/order-items")
+    @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<String> createOrderItem(
-            @AuthenticationPrincipal User user,
             @RequestBody List<OrderItemRequestDto> requestList){
-        Integer userOrStreamerId = Integer.parseInt(user.getUsername());
 
-        String userRole = user.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .findFirst()
-                .orElse("No role assigned");
+        Integer userOrStreamerId = SecurityContextUtil.getUserOrStreamerId();
+        UserRole userRole = SecurityContextUtil.getUserRole();
 
         orderItemService.createOrderItemList(userOrStreamerId, userRole, requestList);
         return ResponseEntity.ok("주문이 완료 되었습니다.");
@@ -38,15 +38,11 @@ public class OrderItemController {
 
     @GetMapping("/order-items")
     public ResponseEntity<Page<OrderItemResponseDto>> getOrderItemList(
-            @AuthenticationPrincipal User user,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "5") int size){
-        Integer userOrStreamerId = Integer.parseInt(user.getUsername());
 
-        String userRole = user.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .findFirst()
-                .orElse("No role assigned");
+        Integer userOrStreamerId = SecurityContextUtil.getUserOrStreamerId();
+        UserRole userRole = SecurityContextUtil.getUserRole();
 
         Pageable pageable = PageRequest.of(page -1 ,size);
 
@@ -55,14 +51,10 @@ public class OrderItemController {
 
     @GetMapping("/order-items/{orderItemId}")
     public ResponseEntity<OrderItemResponseDto> getOrderItem(
-            @AuthenticationPrincipal User user,
             @PathVariable Integer orderItemId){
-        Integer userOrStreamerId = Integer.parseInt(user.getUsername());
 
-        String userRole = user.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .findFirst()
-                .orElse("No role assigned");
+        Integer userOrStreamerId = SecurityContextUtil.getUserOrStreamerId();
+        UserRole userRole = SecurityContextUtil.getUserRole();
 
         return ResponseEntity.ok(orderItemService.getOrderItem(userOrStreamerId, userRole, orderItemId));
     }
