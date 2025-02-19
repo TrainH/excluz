d4751514ad9ff316ff02a94a7d5cc5329bc8256c
임PRO스터\su992 <su9926@naver.com>
2025-02-19 20:22:39 +0900

    :white_check_mark:test:Add unit test for deleteStreamer, updateStreamer success case

--- src/test/java/excluz/excluz/domain/streamer/service/StreamerServiceTest.java
+++ src/test/java/excluz/excluz/domain/streamer/service/StreamerServiceTest.java
@@ -25,9 +25,10 @@ import excluz.excluz.common.datas.SharedData;
 import excluz.excluz.common.entity.Streamer;
 import excluz.excluz.domain.streamer.dto.request.StreamerLoginRequestDto;
 import excluz.excluz.domain.streamer.dto.request.StreamerSignupRequestDto;
+import excluz.excluz.domain.streamer.dto.request.StreamerUpdateRequestDto;
 import excluz.excluz.domain.streamer.dto.response.StreamerLoginResponseDto;
+import excluz.excluz.domain.streamer.dto.response.StreamerResponseDto;
 import excluz.excluz.domain.streamer.repository.StreamerRepository;
-import excluz.excluz.domain.user.enums.UserRole;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("StreamerService")
@@ -85,10 +86,8 @@ class StreamerServiceTest {
 			StreamerLoginResponseDto loginResponseDto = new StreamerLoginResponseDto(token);
 
 			when(streamerRepository.findByEmail(anyString())).thenReturn(Optional.of(SharedData.STREAMER1));
-			when(passwordEncoder.matches(loginRequestDto.getPassword(), SharedData.STREAMER1.getPassword())).thenReturn(
-				true);
-			when(jwtUtil.createToken(SharedData.STREAMER1.getEmail(), SharedData.STREAMER1.getId(),
-				SharedData.STREAMER1.getUserRole())).thenReturn(token);
+			when(passwordEncoder.matches(loginRequestDto.getPassword(), SharedData.STREAMER1.getPassword())).thenReturn(true);
+			when(jwtUtil.createToken(SharedData.STREAMER1.getEmail(), SharedData.STREAMER1.getId(),	SharedData.STREAMER1.getUserRole())).thenReturn(token);
 
 			try (MockedStatic<StreamerLoginResponseDto> mockedStatic = mockStatic(StreamerLoginResponseDto.class)) {
 				given(StreamerLoginResponseDto.from(anyString())).willReturn(loginResponseDto);
@@ -99,8 +98,7 @@ class StreamerServiceTest {
 				// then
 				verify(streamerRepository).findByEmail(loginRequestDto.getEmail());
 				verify(passwordEncoder).matches(loginRequestDto.getPassword(), SharedData.STREAMER1.getPassword());
-				verify(jwtUtil).createToken(SharedData.STREAMER1.getEmail(), SharedData.STREAMER1.getId(),
-					SharedData.STREAMER1.getUserRole());
+				verify(jwtUtil).createToken(SharedData.STREAMER1.getEmail(), SharedData.STREAMER1.getId(), SharedData.STREAMER1.getUserRole());
 				// dto 검증
 				assertNotNull(actualResult);
 				// token 값 검증
@@ -109,5 +107,79 @@ class StreamerServiceTest {
 				assertThat(actualResult.getToken()).startsWith(token);
 			}
 		}
+	}
+
+	@Nested
+	@DisplayName("deleteStreamer 메서드")
+	class DeleteStreamer {
+
+		@Test
+		@DisplayName("success: 스트리머 본인의 계정 탈퇴")
+		void deleteStreamer() {
+			// given
+			Streamer spyStreamer = spy(SharedData.STREAMER1);
+			when(streamerRepository.findById(SharedData.STREAMER_ID1)).thenReturn(Optional.of(spyStreamer));
+			when(passwordEncoder.matches(SharedData.STREAMER_PASSWORD1, spyStreamer.getPassword())).thenReturn(true);
+
+			// 탈퇴 전 스트리머 계정 상태 검증
+			assertThat(spyStreamer.isDeleted()).isEqualTo(false);
+
+			// when
+			streamerService.deleteStreamer(SharedData.STREAMER_ID1, SharedData.STREAMER_PASSWORD1);
+
+			// then
+			// 탈퇴 후 스트리머 계정 상태 검증
+			assertThat(spyStreamer.isDeleted()).isEqualTo(true);
+
+			verify(spyStreamer).updateStreamerStatus(true);
+			verify(streamerRepository).findById(SharedData.STREAMER_ID1);
+			verify(passwordEncoder).matches(SharedData.STREAMER_PASSWORD1, spyStreamer.getPassword());
+		}
+		// 실패: 비밀번호 불일치(=본인의 계정이 아님)인 경우 계정 탈퇴 불가능
+	}
+
+	@Nested
+	@DisplayName("updateStreamer 메서드")
+	class UpdateStreamer {
+
+		@Test
+		@DisplayName("success: 스트리머 본인 정보 수정")
+		void updateStreamer() {
+			// given
+			StreamerUpdateRequestDto requestDto = new StreamerUpdateRequestDto(
+				SharedData.STREAMER_NAME2,
+				SharedData.STREAMER_NICKNAME2,
+				SharedData.STREAMER_PHONE_NUMBER2,
+				SharedData.STREAMER_EMAIL2,
+				SharedData.STREAMER_PASSWORD1);
+			StreamerResponseDto responseDto = new StreamerResponseDto(
+				SharedData.STREAMER_NAME2,
+				SharedData.STREAMER_NICKNAME2,
+				SharedData.STREAMER_PHONE_NUMBER2,
+				SharedData.STREAMER_EMAIL2);
+			Streamer spyStreamer = spy(SharedData.STREAMER1);
+
+			when(streamerRepository.findById(SharedData.STREAMER_ID1)).thenReturn(Optional.of(spyStreamer));
+
+			try (MockedStatic<StreamerResponseDto> mockedStatic = mockStatic(StreamerResponseDto.class)) {
+				given(StreamerResponseDto.from(spyStreamer)).willReturn(responseDto);
+
+				// when
+				StreamerResponseDto actualResult = streamerService.updateStreamer(SharedData.STREAMER_ID1, requestDto);
+
+				// then
+				verify(streamerRepository).findById(SharedData.STREAMER_ID1);
+				verify(spyStreamer).updateStreamer(requestDto.getName(), requestDto.getNickName(), requestDto.getPhoneNumber(), requestDto.getEmail());
+				// Dto 검증
+				assertNotNull(actualResult);
+				// 수정 된 값 검증
+				assertThat(actualResult.getEmail()).isEqualTo(requestDto.getEmail());
+				assertThat(actualResult.getName()).isEqualTo(requestDto.getName());
+				assertThat(actualResult.getNickName()).isEqualTo(requestDto.getNickName());
+				assertThat(actualResult.getPhoneNumber()).isEqualTo(requestDto.getPhoneNumber());
+			}
+		}
+
+		// 실패: 탈퇴한 계정의 정보는 수정할 수 없음
 	}
 }
