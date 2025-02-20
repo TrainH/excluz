341734a2e7af7390eddf5fdf2cab67d0f843e17b
TrainH <ryqja18@gmail.com>
2025-02-20 23:05:40 +0900

    :recycle: refactor: Change pageable setting in OrderItemController

--- src/main/java/excluz/excluz/domain/order/orderItem/controller/OrderItemController.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/controller/OrderItemController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -42,7 +43,7 @@ public class OrderItemController {
         Integer userOrStreamerId = SecurityContextUtil.getUserOrStreamerId();
         UserRole userRole = SecurityContextUtil.getUserRole();
 
-        Pageable pageable = PageRequest.of(page  ,size);
+        Pageable pageable = PageRequest.of(page  ,size, Sort.by(Sort.Order.desc("id")));
 
         return ResponseEntity.ok(orderItemService.getOrderItemList(userOrStreamerId, userRole, pageable));
     }
