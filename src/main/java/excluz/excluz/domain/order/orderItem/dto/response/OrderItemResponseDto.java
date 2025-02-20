b4af99a819e22b521ddbbf7c1cce239f79ad795d
TrainH <ryqja18@gmail.com>
2025-02-20 23:06:32 +0900

    :recycle: refactor: Change OrderItemResponse with id and createdAt

--- src/main/java/excluz/excluz/domain/order/orderItem/dto/response/OrderItemResponseDto.java
+++ src/main/java/excluz/excluz/domain/order/orderItem/dto/response/OrderItemResponseDto.java
@@ -5,32 +5,46 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class OrderItemResponseDto {
 
+    private Integer orderId;
+    private Integer orderItemId;
     private String nickName;
     private Integer itemId;
     private String itemName;
     private Integer price;
     private Integer itemQuantity;
+    private LocalDateTime createdAt;
 
     @Builder
-    public OrderItemResponseDto(String nickName, Integer itemId, String itemName, Integer price, Integer itemQuantity) {
+    public OrderItemResponseDto(Integer orderId, Integer orderItemId,
+                                String nickName, Integer itemId,
+                                String itemName, Integer price,
+                                Integer itemQuantity, LocalDateTime createdAt) {
+        this.orderId = orderId;
+        this.orderItemId = orderItemId;
         this.nickName = nickName;
         this.itemId = itemId;
         this.itemName = itemName;
         this.price = price;
         this.itemQuantity = itemQuantity;
+        this.createdAt = createdAt;
     }
 
     public static OrderItemResponseDto from(OrderItem orderItem) {
         return OrderItemResponseDto.builder()
+                .orderId(orderItem.getOrder().getId())
+                .orderItemId(orderItem.getId())
                 .nickName(orderItem.getItem().getStore().getStreamer().getNickName())
                 .itemId(orderItem.getItem().getId())
                 .itemName(orderItem.getItem().getItemName())
                 .price(orderItem.getItem().getPrice())
                 .itemQuantity(orderItem.getItem_quantity())
+                .createdAt(orderItem.getOrder().getCreatedAt())
                 .build();
     }
 }
