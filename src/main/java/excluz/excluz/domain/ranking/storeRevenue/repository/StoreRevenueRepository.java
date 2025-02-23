7c96c0dca9d78dbc4c83975505aa31962d59b2c3
YJ-Kkang <kyujin995@gmail.com>
2025-02-23 20:40:01 +0900

    :sparkles: feat: Add StoreRevenueRepository for managing store revenue data

---
+++ src/main/java/excluz/excluz/domain/ranking/storeRevenue/repository/StoreRevenueRepository.java
@@ -0,0 +1,8 @@
+package excluz.excluz.domain.ranking.storeRevenue.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import excluz.excluz.common.entity.StoreRevenue;
+
+public interface StoreRevenueRepository extends JpaRepository<StoreRevenue, Integer> {
+}
