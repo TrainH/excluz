4d0aee904001d7265c0f0349813f342625c79e5c
TrainH <ryqja18@gmail.com>
2025-03-07 16:02:50 +0900

    :sparkles: feat: Add OrderV3Service for Cachie

---
+++ src/main/java/excluz/excluz/domain/order/order/service/OrderV3Service.java
@@ -0,0 +1,34 @@
+package excluz.excluz.domain.order.order.service;
+
+import excluz.excluz.common.exception.ForbiddenException;
+import excluz.excluz.common.exception.error.ErrorCode;
+import excluz.excluz.domain.order.order.dto.response.OrderResponseDto;
+import excluz.excluz.domain.order.order.repository.OrderV2Repository;
+import excluz.excluz.domain.user.enums.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderV3Service {
+
+    private final OrderV2Repository orderV2Repository;
+
+    @Cacheable(value = "ORDER_LIST_CACHE", key = "#userOrStreamerId + '_' + #userRole + '_' + #pageable.pageNumber + '_' + #pageable.pageSize")
+    @Transactional(readOnly = true)
+    public Page<OrderResponseDto> getOrderList(Integer userOrStreamerId, UserRole userRole, Pageable pageable) {
+        if (userRole.equals(UserRole.CUSTOMER)) {
+            return orderV2Repository.findByUserId(userOrStreamerId, pageable);
+        }
+
+        if (userRole.equals(UserRole.STREAMER)) {
+            return orderV2Repository.findByStreamerId(userOrStreamerId, pageable);
+        }
+
+        throw new ForbiddenException(ErrorCode.FORBIDDEN_USER_ACCESS);
+    }
+}
