10f64a13863e06b4b746f44f8382c73b700ec2b9
YJ-Kkang <kyujin995@gmail.com>
2025-03-09 01:03:31 +0900

    :sparkles: feat: Add DTOs for store ranking response

---
+++ src/main/java/excluz/excluz/domain/store/storeRanking/dto/response/StoreRankingResponseDto.java
@@ -0,0 +1,19 @@
+package excluz.excluz.domain.store.storeRanking.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class StoreRankingResponseDto {
+	private final Integer storeId; // 스토어 ID
+	private final String storeName; // 스토어 이름
+	private final Long revenue; // 총 매출
+	private final Integer rankPosition; // 순위
+
+	// 매개변수 4개 이하 -> 생성자 직접 작성
+	public StoreRankingResponseDto(Integer storeId, String storeName, Long revenue, Integer rankPosition) {
+		this.storeId = storeId;
+		this.storeName = storeName;
+		this.revenue = revenue;
+		this.rankPosition = rankPosition;
+	}
+}
