eb41594bf18ceaa52dafac85b7d3de5057052d45
임PRO스터\su992 <su9926@naver.com>
2025-03-09 01:14:02 +0900

    :sparkles: feat: Add caching to getItemList for improved performance

---
+++ src/main/java/excluz/excluz/domain/store/item/controller/ItemV3Controller.java
@@ -0,0 +1,34 @@
+package excluz.excluz.domain.store.item.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import excluz.excluz.domain.store.item.dto.response.GetItemListResponseDtoV2;
+import excluz.excluz.domain.store.item.service.ItemV2Service;
+import excluz.excluz.domain.store.item.service.ItemV3Service;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v3/items")
+public class ItemV3Controller {
+
+	private final ItemV3Service itemV3Service;
+
+	@GetMapping()
+	public ResponseEntity<GetItemListResponseDtoV2> getItemList(
+		@RequestParam(defaultValue = "0") Integer minPrice,
+		@RequestParam(defaultValue = "2147483647") Integer maxPrice,
+		@RequestParam(required = false) String itemName,
+		@RequestParam(required = false) Integer storeId,
+		@RequestParam(required = false) Integer cursor,
+		@RequestParam(defaultValue = "20") int limit
+	) {
+		GetItemListResponseDtoV2 responseDto = itemV3Service.getItemList(minPrice, maxPrice, itemName, storeId, cursor, limit);
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/excluz/excluz/domain/store/item/service/ItemV3Service.java
@@ -0,0 +1,57 @@
+package excluz.excluz.domain.store.item.service;
+
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import excluz.excluz.common.entity.Item;
+import excluz.excluz.common.exception.BadRequestException;
+import excluz.excluz.common.exception.error.ErrorCode;
+import excluz.excluz.domain.store.item.dto.response.GetItemListResponseDtoV2;
+import excluz.excluz.domain.store.item.dto.response.ItemListResponseDto;
+import excluz.excluz.domain.store.item.repository.ItemV2Repository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ItemV3Service {
+
+	private final ItemV2Repository itemV2Repository;
+
+	@Cacheable(value = "ITEM_LIST_CACHE",
+		key = "#cursor + '_' + #limit",
+		unless = "#result == null"
+	)
+	@Transactional(readOnly = true)
+	public GetItemListResponseDtoV2 getItemList(
+		Integer minPrice, Integer maxPrice,	String itemName, Integer storeId, Integer cursor, int limit
+	) {
+		if (!isValidPrice(minPrice, maxPrice)) {
+			throw new BadRequestException(ErrorCode.INVALID_ITEM_PRICE);
+		}
+
+		// 다음 페이지 존재 여부 확인
+		List<Item> itemList = itemV2Repository.findByPriceWithItemNameV2(
+			minPrice, maxPrice,	itemName, storeId, cursor, limit + 1);
+
+		boolean hasNext = itemList.size() > limit;
+		if (hasNext) {
+			itemList.remove(itemList.size() - 1); // 다음 페이지 확인용 추가 데이터 제거
+		}
+
+		// 다음 페이지가 있다면 마지막 아이템의 id를 nextCursor로 설정 (정렬 기준 = id 내림차순)
+		Integer nextCursor = hasNext ? itemList.get(itemList.size() - 1).getId() : null;
+
+		List<ItemListResponseDto> itemListResponseDtoList = itemList.stream()
+			.map(ItemListResponseDto::from)
+			.toList();
+
+		return new GetItemListResponseDtoV2(minPrice, maxPrice, itemListResponseDtoList, nextCursor);
+	}
+
+	private boolean isValidPrice(Integer minPrice, Integer maxPrice) {
+		return minPrice >= 0 && maxPrice >= 0 && (maxPrice >= minPrice);
+	}
+}
