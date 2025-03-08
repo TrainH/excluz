5b55b3849a3d41649556b9c0b540c1a3b46b10be
YJ-Kkang <kyujin995@gmail.com>
2025-03-09 01:01:07 +0900

    :sparkles: feat: Implement store ranking history retrieval with dynamic period filtering

--- src/main/java/excluz/excluz/domain/store/storeRanking/repository/StoreRankingRepository.java
+++ src/main/java/excluz/excluz/domain/store/storeRanking/repository/StoreRankingRepository.java
@@ -1,5 +1,7 @@
 package excluz.excluz.domain.store.storeRanking.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import excluz.excluz.common.entity.Store;
 import excluz.excluz.common.entity.StoreRanking;
 import excluz.excluz.domain.store.storeRevenue.enums.RevenuePeriod;
 
@@ -19,5 +22,17 @@ public interface StoreRankingRepository extends JpaRepository<StoreRanking, Long
 	Page<StoreRanking> findTop10ByRankingPeriod(
 		@Param("period") RevenuePeriod period, // 조회할 랭킹 기간 (DAY, MONTH, YEAR)
 		Pageable pageable // 페이지 정보 (여기서는 10개를 요청)
+	);
+
+	// 특정 매장, 랭킹 기간, 날짜 범위에 맞는 정보 조회
+	// 주어진 가게, period, 날짜 범위(startDate ~ endDate)에 해당하는 순위 데이터를 최근 순(날짜 내림차순)으로 조회
+	@Query("SELECT sr FROM StoreRanking sr WHERE sr.store = :store AND "
+		+ "sr.rankingPeriod = :period AND sr.rankDate BETWEEN :startDate AND :endDate ORDER BY sr.rankDate DESC")
+	Page<StoreRanking> findByStoreAndPeriodAndRankDateBetween(
+		@Param("store") Store store, // 조회할 가게
+		@Param("period") RevenuePeriod period, // 조회할 랭킹 기간 (DAY, MONTH, YEAR)
+		@Param("startDate") LocalDateTime startDate, // 검색 시작 날짜
+		@Param("endDate") LocalDateTime endDate, // 검색 종료 날짜
+		Pageable pageable // 페이지 정보 (몇 번째 페이지, 몇 개씩)
 	);
 }
