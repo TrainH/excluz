5757939a941bd6ef70a2e62c830f10a0361ccf43
JunChoi <34149189+Revengersy@users.noreply.github.com>
2025-03-13 17:07:56 +0900

    Update EventApplicantRepository.java

--- src/main/java/excluz/excluz/domain/event/eventApplicant/repository/EventApplicantRepository.java
+++ src/main/java/excluz/excluz/domain/event/eventApplicant/repository/EventApplicantRepository.java
@@ -6,7 +6,8 @@ import excluz.excluz.domain.event.eventApplicant.enums.ApplicantStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
